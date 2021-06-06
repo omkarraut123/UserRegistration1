d349a4d694618317afc3106983e496e3e2d8e230
omkarraut123 <oraut2137@gmail.com>
2021-06-06 20:35:22 +0530

    UserRegistration

---
+++ UserDetail/UserDetail/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserDetail.Models;
+namespace UserDetail.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit()
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "User Registered Successful";
+            return View("AddOrEdit", new User());
+
+        }
+    }
+}
---
+++ UserDetail/UserDetail/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserDetail.MvcApplication" Language="C#" %>
